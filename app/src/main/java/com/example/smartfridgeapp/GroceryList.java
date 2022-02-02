f76e6565902b05076d75fbfcc2ab12a3602186c2
Claire <17cfor@queensu.ca>
2022-02-02 08:35:25 -0500

    reformated json tree and displaying items

---
+++ app/src/main/java/com/example/smartfridgeapp/GroceryList.java
@@ -0,0 +1,24 @@
+package com.example.smartfridgeapp;
+
+public class GroceryList {
+    private String item;
+    private Integer number;
+
+    public GroceryList() {
+
+    }
+
+    public GroceryList(String item, Integer number) {
+        this.item = item;
+        this.number = number;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+}
--- app/src/main/java/com/example/smartfridgeapp/GroceryListActivity.java
+++ app/src/main/java/com/example/smartfridgeapp/GroceryListActivity.java
@@ -3,12 +3,12 @@ package com.example.smartfridgeapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -18,47 +18,28 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class GroceryListActivity extends AppCompatActivity {
-    private Button addItem;
-    private Spinner dropDown;
+    private Button add;
     private ListView listView;
-    List<String> myList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grocery_list);
 
-        addItem = (Button) findViewById(R.id.add);
-        listView = findViewById(R.id.listView);
-        dropDown = findViewById(R.id.spinner);
+        add = findViewById(R.id.add);
+        listView  = findViewById(R.id.listView);
 
-        myList = new ArrayList<>();
-
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(GroceryListActivity.this,android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.groceries));
-        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        dropDown.setAdapter(myAdapter);
-
-        addItem.setOnClickListener(new View.OnClickListener() {
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String txt_name = dropDown.getSelectedItem().toString();
-                if(txt_name.isEmpty()){
-                    Toast.makeText(GroceryListActivity.this,"No Item Entered", Toast.LENGTH_SHORT).show();
-                }else{
-                    myList.add(txt_name);
-                    myList.add(String.valueOf(1));
-                    FirebaseDatabase.getInstance().getReference().child("Grocery List").push().setValue(myList);
-                    myList.clear();
-                }
+
             }
         });
 
         final ArrayList<String> list = new ArrayList<>();
-        final ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.list_item, list);
+        final ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.grocery_item, list);
         listView.setAdapter(adapter);
 
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Grocery List");
@@ -66,19 +47,18 @@ public class GroceryListActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 list.clear();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
-                    list.add(snapshot.getValue().toString());
+                for (DataSnapshot snapshot :dataSnapshot.getChildren()){
+                    GroceryList grocery_items = snapshot.getValue(GroceryList.class);
+                    String txt = grocery_items.getItem() + ": " + grocery_items.getNumber();
+                    list.add(txt);
                 }
                 adapter.notifyDataSetChanged();
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
             }
-
         });
-
 
     }
 }
