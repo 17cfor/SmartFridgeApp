38a57093366b2c17f5bbb9aa3c31dbc4876bbfa4
Areeya T <aim21320@gmail.com>
2022-03-06 15:30:59 -0500

    change firebase realtime database structure(no more FridgeList Class)

--- app/src/main/java/com/example/smartfridgeapp/FridgeActivity.java
+++ app/src/main/java/com/example/smartfridgeapp/FridgeActivity.java
@@ -49,10 +49,9 @@ public class FridgeActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 list.clear();
                 for (DataSnapshot snapshot :dataSnapshot.getChildren()){
-                      list.add(snapshot.getKey() + " : " + snapshot.getValue().toString());
-//                    FridgeList fridge_items = snapshot.getValue(FridgeList.class);
-//                    String txt = fridge_items.getItem() + " : " + fridge_items.getNumber();
-//                    list.add(fridge_items.getItem());
+                    FridgeList fridge_items = snapshot.getValue(FridgeList.class);
+                    String txt = fridge_items.getNumber() + " : " + fridge_items.getItem();
+                    list.add(txt);
                 }
                 adapter.notifyDataSetChanged();
             }
