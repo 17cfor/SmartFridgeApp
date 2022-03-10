c18c36dd92eaf2eab3934084abd51f012ed224b0
Claire <17cfor@queensu.ca>
2022-03-09 20:10:40 -0500

    formatting fix and added listview

--- app/src/main/java/com/example/smartfridgeapp/GroceryListActivity.java
+++ app/src/main/java/com/example/smartfridgeapp/GroceryListActivity.java
@@ -7,8 +7,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -20,6 +22,8 @@ import com.google.firebase.database.ValueEventListener;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class GroceryListActivity extends AppCompatActivity {
 
@@ -29,8 +33,8 @@ public class GroceryListActivity extends AppCompatActivity {
     private EditText orange;
     private EditText tomato;
 
-
     private Button updateGrocery;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,17 +46,22 @@ public class GroceryListActivity extends AppCompatActivity {
         orange = (EditText)findViewById(R.id.numOrange);
         tomato = (EditText)findViewById(R.id.numTomato);
 
-
         updateGrocery = findViewById(R.id.updateGrocery);
+
+        listView  = findViewById(R.id.list_diff);
 
         DatabaseReference g_reference = FirebaseDatabase.getInstance().getReference().child("Grocery List");
         DatabaseReference f_reference = FirebaseDatabase.getInstance().getReference().child("Fridge");
-
+       // DatabaseReference d_reference = FirebaseDatabase.getInstance().getReference().child("Difference");
 
         int[] groceryNumbers = {0,0,0,0,0}; //there needs to be a zero for each item being tracked
 
-        HashMap<String, String> groceryList = new HashMap<String, String>();
-        HashMap<String, String> fridgeList = new HashMap<String, String>();
+        Map<String, String> groceryList = new TreeMap<String, String>();
+        Map<String, String> fridgeList = new TreeMap<String, String>();
+
+        final ArrayList<String> list = new ArrayList<>();
+        final ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.diff_item, list);
+        listView.setAdapter(adapter);
 
         g_reference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -63,6 +72,7 @@ public class GroceryListActivity extends AppCompatActivity {
                 updateGrocery.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        list.clear();
                         // get inputs and convert to int
                         groceryNumbers[0] = Integer.parseInt(apple.getText().toString());
                         groceryNumbers[1] = Integer.parseInt(banana.getText().toString());
@@ -74,12 +84,21 @@ public class GroceryListActivity extends AppCompatActivity {
                         for (DataSnapshot snapshot :dataSnapshot.getChildren()){
 
                             g_reference.child(snapshot.getKey()).setValue(String.valueOf(groceryNumbers[i]));
-
-
+                            groceryList.put(String.valueOf(snapshot.getKey()),String.valueOf(groceryNumbers[i]));
                             i += 1;
                         }
                         updateList(dataSnapshot);
+                        //compareLists(groceryList, fridgeList);
+                        Map<String, String> difference =  compareLists(groceryList, fridgeList);
 
+                        for(Map.Entry<String, String> dEntry : difference.entrySet())
+                        {
+                            //gEntry.getKey());
+                           // Log.d("G Val", String.valueOf(dEntry.getValue()));
+                            list.add(dEntry.getKey() + " : " + dEntry.getValue().toString());
+                        }
+
+                        adapter.notifyDataSetChanged();
                     }
                 });
             }
@@ -94,7 +113,9 @@ public class GroceryListActivity extends AppCompatActivity {
         f_reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
+                for (DataSnapshot snapshot :dataSnapshot.child("food").getChildren()){
+                    fridgeList.put(String.valueOf(snapshot.getKey()), String.valueOf(snapshot.getValue()));
+                }
             }
 
             @Override
@@ -112,7 +133,40 @@ public class GroceryListActivity extends AppCompatActivity {
         carrot.setText(dataSnapshot.child("carrot").getValue(String.class));
         orange.setText(dataSnapshot.child("orange").getValue(String.class));
         tomato.setText(dataSnapshot.child("tomato").getValue(String.class));
+    }
 
+    public Map<String, String> compareLists(Map<String, String> groceryList, Map<String, String> fridgeList)
+    {
+        Map<String, String> difference = new TreeMap<String, String>();
+        int temp;
+        for(Map.Entry<String, String> gEntry : groceryList.entrySet())
+        {
+            // Log.d("G Key", gEntry.getKey());
+             //Log.d("G Val", gEntry.getValue());
+
+            for(Map.Entry<String, String> fEntry : fridgeList.entrySet())
+            {
+                if(gEntry.getKey().equals(fEntry.getKey())) // if there is an item in both fridge and grocery list
+                {
+                    //Log.d("Tag", gEntry.getValue());
+                    //return str.substring(0, 1).toUpperCase() + str.substring(1);
+                    temp = Integer.parseInt(gEntry.getValue()) - Integer.parseInt(fEntry.getValue());
+                   if( temp > 0 )
+                    {
+                        difference.put(gEntry.getKey().substring(0, 1).toUpperCase() + gEntry.getKey().substring(1), String.valueOf(temp));
+                    }
+                   else
+                       {
+                        difference.put(gEntry.getKey().substring(0, 1).toUpperCase() + gEntry.getKey().substring(1), String.valueOf(0));
+                   }
+
+                    //list.add(gEntry.getKey(), (Integer.parseInt(gEntry.getValue()) - Integer.parseInt(fEntry.getValue()) ));
+                    //Integer.parseInt(onion.getText().toString());
+                }
+            }
+        }
+
+        return difference;
     }
 
     }
