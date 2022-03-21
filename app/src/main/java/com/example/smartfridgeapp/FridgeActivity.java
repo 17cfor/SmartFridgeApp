4c25622977395241a2baeaf3fb027b83e37e3fd1
Areeya T <aim21320@gmail.com>
2022-03-20 20:05:08 -0400

    add image

--- app/src/main/java/com/example/smartfridgeapp/FridgeActivity.java
+++ app/src/main/java/com/example/smartfridgeapp/FridgeActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,6 +22,7 @@ import java.util.ArrayList;
 public class FridgeActivity extends AppCompatActivity {
     private Button back;
     private ListView listView;
+//    private Button refresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,7 @@ public class FridgeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fridge);
 
         back = findViewById(R.id.back);
+//        refresh = findViewById(R.id.refresh);
         listView  = findViewById(R.id.listView);
 
         back.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +39,13 @@ public class FridgeActivity extends AppCompatActivity {
                 startActivity(new Intent(FridgeActivity.this, MainActivity.class));
             }
         });
+//
+//        refresh.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(FridgeActivity.this, FridgeActivity.class));
+//            }
+//        });
 
         final ArrayList<String> list = new ArrayList<>();
         final ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.fridge_item, list);
@@ -48,9 +58,9 @@ public class FridgeActivity extends AppCompatActivity {
                 list.clear();
                 for (DataSnapshot snapshot :dataSnapshot.child("food").getChildren()){
                     list.add(snapshot.getKey().substring(0, 1).toUpperCase() + snapshot.getKey().substring(1)  + " : " + snapshot.getValue().toString());
-//                    FridgeList fridge_items = snapshot.getValue(FridgeList.class);
-//                    String txt = fridge_items.getNumber() + " : " + fridge_items.getItem();
-//                    list.add(txt);
+                    for (DataSnapshot snapshot2 :dataSnapshot.child("non_food").getChildren()){
+                        Toast.makeText(FridgeActivity.this, "Warning: there is a phone in the fridge", Toast.LENGTH_SHORT).show();
+                    }
                 }
 
                 adapter.notifyDataSetChanged();
